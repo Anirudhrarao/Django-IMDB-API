82e105710633011b12f011f651dbd7454150822b
Anirudhrarao <raorudhra16@gmail.com>
2023-10-29 10:34:43 +0530

    Class based view using APIView class

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from watchlist.views import movie_list, movie_details
+from watchlist.views import MovieListView, MovieDetailView
 
 urlpatterns = [
-    path('', movie_list, name = 'watch-list'),    
-    path('details/<int:pk>', movie_details, name = 'movie-detail'),    
+    path('', MovieListView.as_view(), name = 'watch-list'),    
+    path('details/<int:pk>', MovieDetailView.as_view(), name = 'movie-detail'),    
 ]
 
 
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,80 +1,107 @@
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status 
 from watchlist.models import Movie
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from watchlist.serializers import MovieSerializer
-from rest_framework.decorators import api_view
 
-@api_view(['GET', 'POST'])
-def movie_list(request):
+class MovieListView(APIView):
     """
-    Retrieve a list of movies or create a new movie entry.
+    View for listing and creating movies.
 
-    This view supports both retrieving a list of all movies and creating a new movie entry. GET requests retrieve movie data, while POST requests create a new movie entry.
+    This view supports both retrieving a list of all movies and creating a new movie entry.
 
-    Args:
-        request: HTTP request object.
-
-    Returns:
-        Response: Serialized movie data as a JSON response for GET requests or a success response for POST requests. In case of an exception, it returns an error response.
-
-    Raises:
-        Exception: If there is an error during the process.
+    Attributes:
+        serializer_class: The serializer class for Movie objects.
     """
-    if request.method == 'GET':
-        try:
-            queryset = Movie.objects.all()
-            serializer = MovieSerializer(queryset, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            error_message = "An error occurred while retrieving movie data."
-            return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
-    if request.method == 'POST':
-        serializer = MovieSerializer(data=request.data)
+    serializer_class = MovieSerializer
+
+    def get(self, request):
+        """
+        Retrieve a list of movies.
+
+        Args:
+            request: HTTP request object.
+
+        Returns:
+            Response: Serialized movie data as a JSON response.
+        """
+        queryset = Movie.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Create a new movie entry.
+
+        Args:
+            request: HTTP request object.
+
+        Returns:
+            Response: Serialized movie data as a JSON response or an error response in case of validation failure.
+        """
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_details(request, pk):
+class MovieDetailView(APIView):
     """
-    Retrieve details of a specific movie, update, or delete it.
+    View for retrieving, updating, or deleting a movie.
 
-    This view retrieves details of a movie with the specified primary key (pk) from the database and serializes it using MovieSerializer. It also supports updating or deleting the movie.
+    This view supports retrieving details, updating, and deleting a specific movie.
 
-    Args:
-        request: HTTP request object.
-        pk: Primary key of the movie to retrieve, update, or delete.
-
-    Returns:
-        Response: Serialized movie data as a JSON response for GET requests, or a success response for PUT requests, or a success response for DELETE requests. In case of an exception, it returns an error response.
-
-    Raises:
-        Exception: If there is an error during the process.
+    Attributes:
+        serializer_class: The serializer class for Movie objects.
     """
-    try:
-        movie = Movie.objects.get(pk=pk)
-        if request.method == 'GET':
-            serializer = MovieSerializer(movie)
+    serializer_class = MovieSerializer
+
+    def get(self, request, pk):
+        """
+        Retrieve details of a specific movie.
+
+        Args:
+            request: HTTP request object.
+            pk: Primary key of the movie to retrieve.
+
+        Returns:
+            Response: Serialized movie data as a JSON response or an error response in case of a not found exception.
+        """
+        movie = get_object_or_404(Movie, pk=pk)
+        serializer = self.serializer_class(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        """
+        Update details of a specific movie.
+
+        Args:
+            request: HTTP request object.
+            pk: Primary key of the movie to update.
+
+        Returns:
+            Response: Serialized movie data as a JSON response after update or an error response in case of validation failure.
+        """
+        movie = get_object_or_404(Movie, pk=pk)
+        serializer = self.serializer_class(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        if request.method == 'PUT':
-            serializer = MovieSerializer(movie, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        if request.method == 'DELETE':
-            movie.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    except Movie.DoesNotExist:
-        return Response({"error": "Movie not found."}, status=status.HTTP_404_NOT_FOUND)
-    
-    except Exception as e:
-        error_message = "An error occurred while processing the movie request."
-        return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Delete a specific movie.
+
+        Args:
+            request: HTTP request object.
+            pk: Primary key of the movie to delete.
+
+        Returns:
+            Response: A success response indicating the movie has been deleted.
+        """
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)       
