5e03aa5fddb8e51d9378081c11105ac82a21493f
Anirudhrarao <raorudhra16@gmail.com>
2023-10-31 11:26:59 +0530

    Nested relationship

--- watchlist/serializers.py
+++ watchlist/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
 from watchlist.models import WatchList, StreamPlatform
 
-class WatchListSerializer(serializers.ModelSerializer):
+class WatchListSerializer(serializers.ModelSerializer): 
     
     class Meta:
         model = WatchList
         fields = "__all__"
     
-
 class StreamPlatformSerializer(serializers.ModelSerializer):
+    # watchlist is name which is given in foreign key as related name
+    watchlist = WatchListSerializer(many=True, read_only=True)
     
     class Meta:
         model = StreamPlatform
-        fields = "__all__"
+        fields = "__all__"        
+
+
         
